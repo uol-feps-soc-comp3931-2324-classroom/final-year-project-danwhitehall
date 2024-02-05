475dbbb3406c64db3484b96b5313af6f77f04d3c
danwhitehall <sc21djw@leeds.ac.uk>
2024-02-05 16:31:27 +0000

    experimentingwith edge detection and regions of interest

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # imports
 import sys
+import cv2 as cv
+import numpy as np
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QPixmap, QAction, QIcon, QImage, QPalette
-from PyQt6.sip import voidptr
+# from PyQt6.sip import voidptr
 from file_info import fileInfo
+
 
 
 # main window class
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.image = None
 
         # set all properties
         self.setObjectName("main")
@@ -89,9 +93,18 @@ class MainWindow(QMainWindow):
     def createSideMenu(self):
         self.side_menu = QDockWidget("Side Menu")
         self.side_menu.setAllowedAreas(Qt.DockWidgetArea.LeftDockWidgetArea | Qt.DockWidgetArea.RightDockWidgetArea)
-        self.side_menu.setMinimumWidth(100)
+        self.side_menu.setMinimumWidth(200)
 
+        find_areas = QToolButton(self)
+        find_areas.setToolTip("Find areas in image")
+        find_areas.clicked.connect(self.find_areas_click)
+
+        side_grid = QGridLayout()
+        side_grid.addWidget(find_areas, 0, 0)
+        side_grid.setRowStretch(7,10)
+    
         container = QWidget()
+        container.setLayout(side_grid)
 
         self.side_menu.setWidget(container)
 
@@ -110,15 +123,87 @@ class MainWindow(QMainWindow):
         if dialogSucessful:
             # get the selected file
             fileInfo.selectedFiles = dialog.selectedFiles()
+            self.image_path = fileInfo.selectedFiles[0]
 
             # set whats in big image to the selected file
-            self.big_image_label.image = QImage(fileInfo.selectedFiles[0])
+            self.big_image_label.image = QImage(self.image_path)
             self.big_image_label.setPixmap(QPixmap().fromImage(self.big_image_label.image))
             self.big_image_label.resize(self.big_image_label.pixmap().size())
          
         # if the user didn't select a file then print this
         else:
             print("User called file dialog")
+
+    def find_areas_click(self):
+        if not self.big_image_label.image.isNull():
+            img = cv.imread(self.image_path)
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            blur = cv.medianBlur(gray, 5)
+
+            sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+            sharpen = cv.filter2D(blur, -1, sharpen_kernel)
+
+            canny = cv.Canny(sharpen, 0, 255, 500)
+            contours, _ = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            
+            areas = []
+            for cnt in contours:
+                area = cv.contourArea(cnt)
+                areas.append(area)
+            
+            print(areas)
+            max_index = np.argmax(areas)
+            x, y, w, h = cv.boundingRect(contours[max_index])
+            roi = img[y : y + h , x : x + w]
+            cv.imshow('ROI',roi)
+            
+
+            cv.imshow('canny', canny)
+
+
+            # Threshold and morph close
+            thresh = cv.threshold(sharpen, 160, 255, cv.THRESH_BINARY_INV)[1]
+            kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+            close = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=2)
+
+            # Find contours and filter using threshold area
+            cnts = cv.findContours(close, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+            min_area = 100
+            max_area = 1500
+            image_number = 0
+            for c in cnts:
+                area = cv.contourArea(c)
+                print(area)
+                if area > min_area and area < max_area:
+                    x,y,w,h = cv.boundingRect(c)
+                    ROI = img[y:y+h, x:x+w]
+                    cv.imwrite('ROI_{}.png'.format(image_number), ROI)
+                    cv.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
+                    image_number += 1
+
+            # cv.imshow('sharpen', sharpen)
+            # cv.imshow('close', close)
+            # cv.imshow('thresh', thresh)
+            # cv.imshow('image', img)
+            # cv.waitKey()
+            print(image_number)
+
+            # canny = cv.Canny(bilatBlur, 0, 255, 500)
+
+            # height, width, channel = canny.shape
+            # bytes_per_line = width*channel
+            # self.big_image_label.image = QImage(canny.data, width, height, bytes_per_line, QImage.Format.Format_Grayscale8)
+            # self.big_image_label.setPixmap(QPixmap().fromImage(self.big_image_label.image))
+            # self.big_image_label.resize(self.big_image_label.pixmap().size())
+
+
+
+
+            
+            
+
 
 
 
